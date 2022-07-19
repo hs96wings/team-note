58b1006f186e0af6f77c120e4a7247e314221028
hs96wings <hs96wings@gmail.com>
2022-07-19 21:40:21 +0900

    탐색

--- python/lcm_gcd.py
+++ python/lcm_gcd.py
@@ -8,3 +8,13 @@ b = 14
 
 print(math.gcd(21, 14)) # GCD
 print(lcm(21, 14)) # LCM
+
+
+# Recursive
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+
+print(gcd(192, 162))
